98cf61e7746b02841ad6c4ab2e8bff16423863d3
TalalMiftah <64993605+TalalMiftah@users.noreply.github.com>
2023-07-18 09:43:03 +0100

    talal

--- ex01/test.cpp
+++ ex01/test.cpp
@@ -3,45 +3,53 @@
 #include <stdio.h>
 #include <string>
 
+
 class contact {
-	public :
-		int			index;
-		std::string	firstName;
-		std::string	lastName;
-		std::string	nickName;
-		std::string	phoneNumber;
+	int			index;
+	std::string	firstName;
+	std::string	lastName;
+	std::string	nickName;
+	std::string	phoneNumber;
 };
+
+class Phonebook {
+	public:
+		contact contact[8];
+};
+
+
+// void	helpMe()
+// {
+	
+// }
+
+std::string	print(std::string str)
+{
+	if (str.length() > 10)
+	{
+		str = str.substr(0, 9);
+		str += ".";
+	}
+	else
+		str.resize(10 , ' ');
+	return (str);
+}
 
 void	printThatShit(contact *pb)
 {
 	std::string substring;
-	std::cout << "\n|---------------------------------------------------------------------------------------|\n";
-	std::cout << "|\tindex\t|\tfirst name\t|\tlast name\t|\tnickname\t|\n";
-	std::cout << "|---------------------------------------------------------------------------------------|\n";
+	std::cout << "|-------------------------------------------|\n";
+	std::cout << "|    id    |first name|last name | nickname |\n";
+	std::cout << "|-------------------------------------------|\n";
 	for (int i = 0; i < 8; i++)
 	{
 		if (pb[i].firstName[0])
 		{
-			std::cout << "|\t" << pb[i].index << "\t";
-			if (pb[i].firstName.length() >= 9) {
-				substring = pb[i].firstName.substr(0, 10);
-				std::cout << "|\t" << substring << ".\t|";
-    		}
-			else
-				std::cout << "|\t" << std::setw(10) << pb[i].firstName << "\t|";
-			if (pb[i].lastName.length() >= 9) {
-        		substring = pb[i].lastName.substr(0, 10);
-				std::cout << "|\t" << substring << ".\t";
-    		}
-			else
-				std::cout << "\t" << pb[i].lastName << "\t";
-			if (pb[i].nickName.length() >= 9) {
-        		substring = pb[i].nickName.substr(0, 10);
-				std::cout << "|\t" << substring << ".\t|\n";
-    		}
-			else
-				std::cout << "\t" << pb[i].nickName << "\t|\n";
-			std::cout << "|---------------------------------------------------------------------------------------|\n";
+			std::cout << "|    " << pb[i].index << "     ";
+			std::cout << "|" << print(pb[i].firstName) << "|";
+			std::cout << "" << print(pb[i].lastName) << "|";
+			std::cout << "" << print(pb[i].nickName) << "|\n";
+			std::cout << "|-------------------------------------------|\n";
 		}
 	}
 	
@@ -55,13 +63,17 @@ void	addContact(contact *pb)
 	pb[i].index = i;
 	std::cout << "-------------\n";
 	std::cout << "first name : ";
-	std::getline(std::cin, pb[i].firstName);
+	if (!std::getline(std::cin, pb[i].firstName))
+		exit (0);
 	std::cout << "last name : ";
-	std::getline(std::cin, pb[i].lastName);
+	if (!std::getline(std::cin, pb[i].lastName))
+		exit (0);
 	std::cout << "nickname : ";
-	std::getline(std::cin, pb[i].nickName);
+	if (!std::getline(std::cin, pb[i].nickName))
+		exit (0);
 	std::cout << "phone number : ";
-	std::getline(std::cin, pb[i].phoneNumber);
+	if (!std::getline(std::cin, pb[i].phoneNumber))
+		exit (0);
 	i++;
 }
 
@@ -76,10 +88,11 @@ int main ()
 	contact pb[8];
 	while (1)
 	{
-		std::cout << "\n---------------------------------------------\n";
+		std::cout << "---------------------------------------------\n";
 		std::cout << "Enter one of this Command ADD | SEARCH | EXIT\n";
-		std::cout << "---------------------------------------------\n\n";
-		std::getline(std::cin, str);
+		std::cout << "---------------------------------------------\n";
+		if (!std::getline(std::cin, str))
+			exit(0);
 		if (str == "EXIT" || str.empty())
 			exit(0);
 		if (str == "ADD")
