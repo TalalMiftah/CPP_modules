b153cd72ae4e73ee4952e3f2e2a05378bd60cab3
TalalMiftah <talalmiftahvrry@gmail.com>
2023-07-18 11:31:46 +0100

    done

--- ex01/test.cpp
+++ ex01/test.cpp
@@ -3,18 +3,75 @@
 #include <stdio.h>
 #include <string>
 
+#define str_ std::string
+#define Fname 1
+#define Lname 2
+#define Nname 3
+#define Pnum 4
 
 class contact {
-	int			index;
-	std::string	firstName;
-	std::string	lastName;
-	std::string	nickName;
-	std::string	phoneNumber;
+	private:
+		int			index;
+		std::string	firstName;
+		std::string	lastName;
+		std::string	nickName;
+		std::string	phoneNumber;
+	public :
+		void	setindex(int i) {
+			index = i;
+		}
+		void	setfn(str_ name) {
+			firstName = name;
+		}
+		void	setlastn(str_ lname) {
+			lastName = lname;
+		}
+		void	setnickn(str_ nname) {
+			nickName = nname;
+		}
+		void	setPhone(str_ phone) {
+			phoneNumber = phone;
+		}
+		str_	getContactinfo(int macro) {
+			if (macro == 1)
+				return firstName;
+			if (macro == 2)
+				return lastName;
+			if (macro == 3)
+				return nickName;
+			if (macro == 4)
+				return phoneNumber;
+			return NULL;
+		}
+		int		getid() {
+			return index;
+		}
 };
 
 class Phonebook {
 	public:
-		contact contact[8];
+		contact conta_[8];
+		void	setindex (int index, int i) {
+			conta_[index].setindex(i);
+		}
+		void	setFirstName (int index, str_ name) {
+			conta_[index].setfn(name);
+		}
+		void	setLastName (int index, str_ lname) {
+			conta_[index].setlastn(lname);
+		}
+		void	setnickname (int index, str_ nickname) {
+			conta_[index].setnickn(nickname);
+		}
+		void	setPhoneNumber (int index, str_ phone) {
+			conta_[index].setPhone(phone);
+		}
+		str_	getContact (int index, int macro) {
+			return conta_[index].getContactinfo(macro);
+		}
+		int		getIndex(int index) {
+			return conta_[index].getid();
+		}
 };
 
 
@@ -23,7 +80,7 @@ class Phonebook {
 	
 // }
 
-std::string	print(std::string str)
+std::string	print(str_ str)
 {
 	if (str.length() > 10)
 	{
@@ -35,7 +92,7 @@ std::string	print(std::string str)
 	return (str);
 }
 
-void	printThatShit(contact *pb)
+void	printThatShit(Phonebook *pb)
 {
 	std::string substring;
 	std::cout << "|-------------------------------------------|\n";
@@ -43,49 +100,69 @@ void	printThatShit(contact *pb)
 	std::cout << "|-------------------------------------------|\n";
 	for (int i = 0; i < 8; i++)
 	{
-		if (pb[i].firstName[0])
+		if (pb->getContact(i, 1)[0])
 		{
-			std::cout << "|    " << pb[i].index << "     ";
-			std::cout << "|" << print(pb[i].firstName) << "|";
-			std::cout << "" << print(pb[i].lastName) << "|";
-			std::cout << "" << print(pb[i].nickName) << "|\n";
+			std::cout << "|    " << pb->getIndex(i) << "     ";
+			std::cout << "|" << print(pb->getContact(i, 1)) << "|";
+			std::cout << "" << print(pb->getContact(i, 2)) << "|";
+			std::cout << "" << print(pb->getContact(i, 3)) << "|\n";
 			std::cout << "|-------------------------------------------|\n";
 		}
 	}
 	
 }
 
-void	addContact(contact *pb)
+int	chechPhoneNumber(str_ phone)
+{
+	int i = 0;
+
+	while (phone[i])
+	{
+		if (isdigit(phone[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	addContact(Phonebook *pb)
 {
 	static int i;
+	str_ tmp;
 	if (i == 8)
 		i = 0;
-	pb[i].index = i;
+	pb->setindex(i, i);
 	std::cout << "-------------\n";
 	std::cout << "first name : ";
-	if (!std::getline(std::cin, pb[i].firstName))
+	if (!std::getline(std::cin, tmp))
 		exit (0);
+	pb->setFirstName(i, tmp);
 	std::cout << "last name : ";
-	if (!std::getline(std::cin, pb[i].lastName))
+	if (!std::getline(std::cin, tmp))
 		exit (0);
+	pb->setLastName(i, tmp);
 	std::cout << "nickname : ";
-	if (!std::getline(std::cin, pb[i].nickName))
+	if (!std::getline(std::cin, tmp))
 		exit (0);
+	pb->setnickname(i, tmp);
 	std::cout << "phone number : ";
-	if (!std::getline(std::cin, pb[i].phoneNumber))
+	if (!std::getline(std::cin, tmp))
 		exit (0);
+	if (!chechPhoneNumber(tmp))
+		pb->setPhoneNumber(i, tmp);
 	i++;
 }
 
-void	searchContact(contact *pb)
+void	searchContact(Phonebook *pb)
 {
 	printThatShit(pb);
 }
 
 int main ()
 {
-	std::string	str;
-	contact pb[8];
+	str_		str;
+	Phonebook	pb;
+
 	while (1)
 	{
 		std::cout << "---------------------------------------------\n";
@@ -96,8 +173,8 @@ int main ()
 		if (str == "EXIT" || str.empty())
 			exit(0);
 		if (str == "ADD")
-			addContact(pb);
+			addContact(&pb);
 		if (str == "SEARCH")
-			searchContact(pb);
+			searchContact(&pb);
 	}
 }
