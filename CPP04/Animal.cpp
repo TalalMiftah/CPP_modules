cd9bd2cf1bedc5e99d59a95eaaaa7c16cf19352c
TalalMiftah <talalmiftahvrry@gmail.com>
2023-10-03 22:45:36 +0100

    done in the god willing

---
+++ CPP04/Animal.cpp
@@ -0,0 +1,17 @@
+#include "Animal.hpp"
+
+Animal::Animal(void) {
+    
+}
+
+Animal::Animal(const Animal& animal) {
+    
+}
+
+Animal& Animal::operator=(const Animal& animal) {
+    
+}
+
+Animal::~Animal() {
+    
+}
---
+++ CPP04/Animal.hpp
@@ -0,0 +1,18 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+#include <iostream>
+#include <string>
+
+class Animal
+{
+    protected:
+        std::string type;
+    public:
+        Animal(void);
+        Animal(const Animal& animal);
+        Animal& operator=(const Animal& animal);
+        ~Animal();
+};
+
+
+#endif
